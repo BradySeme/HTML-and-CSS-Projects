8749afee37bb5835a44f54593872139c9eb55778
BradySeme <107603832+BradySeme@users.noreply.github.com>
2022-06-26 22:48:36 -0600

    Assignment Changes

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("You dropped something on your shirt.");
+alert ("Haha made you look.");
+function My_First_Function() {
+	var String = "Kiss me, I'm Irish!";
+	var result = String.fontcolor("green");
+	document.getElementById("Irish").innerHTML =
+	result;
+	}
